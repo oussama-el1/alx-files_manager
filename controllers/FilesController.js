cc0f1f3c96a62d3a795101b495f6edd785d8a97f
oussama-el1 <oelhadraoui284@gmail.com>
2024-08-05 16:41:02 +0100

    solve

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -19,85 +19,79 @@ const WriteDataToFile = async (dir, file, Base64Data) => {
 
 class FilesController {
   static async postUpload(req, res) {
-    const xtoken = req.headers['x-token'];
-    const UserId = await redisClient.get(`auth_${xtoken}`);
-    const user = await dbClient.UserByid(UserId);
+    try {
+      const xtoken = req.headers['x-token'];
+      const UserId = await redisClient.get(`auth_${xtoken}`);
+      const user = await dbClient.UserByid(UserId);
 
-    if (!user) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-
-    const {
-      name, type, data,
-    } = req.body;
-
-    let isPublic;
-    let parentId;
-
-    if (!req.body.parentId) {
-      parentId = 0;
-    } else {
-      parentId = req.body.parentId;
-    }
-
-    if (!req.body.isPublic) {
-      isPublic = false;
-    } else {
-      isPublic = req.body.isPublic;
-    }
-
-    if (!name) {
-      return res.status(400).json({ error: 'Missing name' });
-    }
-
-    if (!type || !AcceptedTypes.includes(type)) {
-      return res.status(400).json({ error: 'Missing type' });
-    }
-
-    if (!data && type !== 'folder') {
-      return res.status(400).json({ error: 'Missing data' });
-    }
-
-    if (parentId) {
-      const parentFile = await dbClient.FileByid(parentId);
-      if (!parentFile) {
-        return res.status(400).json({ error: 'Parent not found' });
+      if (!user) {
+        return res.status(401).json({ error: 'Unauthorized' });
       }
 
-      if (parentFile.type !== 'folder') {
-        return res.status(400).json({ error: 'Parent is not a folder' });
-      }
-    }
+      const {
+        name, type, data, parentId = 0, isPublic = false,
+      } = req.body;
 
-    if (type === 'folder') {
-      const newFolder = await dbClient.addFile(name, UserId, type, parentId, isPublic, null);
+      if (!name) {
+        return res.status(400).json({ error: 'Missing name' });
+      }
+
+      if (!type || !AcceptedTypes.includes(type)) {
+        return res.status(400).json({ error: 'Missing type' });
+      }
+
+      if (!data && type !== 'folder') {
+        return res.status(400).json({ error: 'Missing data' });
+      }
+
+      if (parentId) {
+        const parentFile = await dbClient.FileByid(parentId);
+        if (!parentFile) {
+          return res.status(400).json({ error: 'Parent not found' });
+        }
+
+        if (parentFile.type !== 'folder') {
+          return res.status(400).json({ error: 'Parent is not a folder' });
+        }
+      }
+
+      if (type === 'folder') {
+        const newFolder = await dbClient.addFile(name, UserId, type, parentId, isPublic, null);
+        return res.status(201).json({
+          id: newFolder.insertedId,
+          userId: UserId,
+          name,
+          type,
+          isPublic,
+          parentId,
+        });
+      }
+
+      const FOLDER_PATH = process.env.FOLDER_PATH || '/tmp/files_manager';
+      const FILE_NAME = uuid4();
+      const localPath = `${FOLDER_PATH}/${FILE_NAME}`;
+
+      try {
+        await WriteDataToFile(FOLDER_PATH, localPath, data);
+        console.log('File Created successfully');
+      } catch (err) {
+        console.error('Error creating file: ', err);
+        return res.status(500).json({ error: 'Error creating file' });
+      }
+
+      const newFile = await dbClient.addFile(name, UserId, type, parentId, isPublic, localPath);
       return res.status(201).json({
-        id: newFolder.insertedId,
+        id: newFile.insertedId,
         userId: UserId,
         name,
         type,
         isPublic,
         parentId,
       });
+    } catch (error) {
+      console.error('Internal server error: ', error);
+      return res.status(500).json({ error: 'Internal server error' });
     }
-
-    const FOLDER_PATH = process.env.FOLDER_PATH || '/tmp/files_manager';
-    const FILE_NAME = uuid4();
-    const localPath = `${FOLDER_PATH}/${FILE_NAME}`;
-
-    await WriteDataToFile(FOLDER_PATH, localPath, data)
-      .then(() => console.log('File Created succesfuly'))
-      .catch((err) => console.error('error creating File: ', err));
-
-    const newFile = await dbClient.addFile(name, UserId, type, parentId, isPublic, localPath);
-    return res.status(201).json({
-      id: newFile.insertedId,
-      userId: UserId,
-      name,
-      type,
-      isPublic,
-      parentId,
-    });
   }
 }
 
